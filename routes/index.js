22554212e0530e07be91b4b1f3d913a8dbf7082d
Devin McInnis <devin.mcinnis@gmail.com>
2017-11-29 21:30:29 -0800

    attach end time to all kitties; refactor kitty to mixin

--- routes/index.js
+++ routes/index.js
@@ -3,50 +3,39 @@ var router = express.Router();
 var request = require('request');
 var moment = require('moment');
 
-var ethPrice = 520;
+function attachTimeUntilEnd(cat) {
+  var timeNow = moment();
+  var catEndTime = moment(new Date(parseInt(cat.end_time, 10)))
+  cat.time_until_end = catEndTime.fromNow();
+
+  var endingWithin5Minutes = catEndTime.diff(timeNow, 'minutes') <= 30;
+
+  if (endingWithin5Minutes) {
+    cat.ending_soon = true;
+  }
+
+  return cat;
+}
 
 function getEthPrice() {
-  request('https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=CAD', function(err, resp) {
-    ethPrice = parseInt(JSON.parse(resp.body)[0].price_cad, 10);
+  request('https://api.coinmarketcap.com/v1/ticker/ethereum/', function(err, resp) {
+    ethPrice = parseInt(JSON.parse(resp.body)[0].price_usd, 10);
   });
 }
 
+var ethPrice = 0;
 getEthPrice();
 setInterval(getEthPrice, 1000 * 60 * 15); // Every 15 minutes
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  request('https://api.cryptokitties.co/auctions?offset=&limit=1000&type=sale&status=open', function(err, respSale) {
-    request('https://api.cryptokitties.co/auctions?offset=&limit=1000&type=sire&status=open', function(err, respSire) {
+  request('https://api.cryptokitties.co/auctions?offset=0&limit=1000&type=sale&status=open', function(err, respSale) {
+    request('https://api.cryptokitties.co/auctions?offset=0&limit=1000&type=sire&status=open', function(err, respSire) {
 
-      var saleAuctions = JSON.parse(respSale.body).auctions.map(function(cat) {
-        var timeNow = moment();
-        var catEndTime = moment(new Date(parseInt(cat.end_time, 10)))
-        cat.time_until_end = catEndTime.fromNow();
+      var saleAuctions = JSON.parse(respSale.body).auctions.map(attachTimeUntilEnd);
 
-        var endingWithin5Minutes = catEndTime.diff(timeNow, 'minutes') <= 30;
-
-        if (endingWithin5Minutes) {
-          cat.ending_soon = true;
-        }
-
-        return cat;
-      });
-
-      var sireAuctions = JSON.parse(respSire.body).auctions.map(function(cat) {
-        var timeNow = moment();
-        var catEndTime = moment(new Date(parseInt(cat.end_time, 10)))
-        cat.time_until_end = catEndTime.fromNow();
-
-        var endingWithin5Minutes = catEndTime.diff(timeNow, 'minutes') <= 30;
-
-        if (endingWithin5Minutes) {
-          cat.ending_soon = true;
-        }
-
-        return cat;
-      });
+      var sireAuctions = JSON.parse(respSire.body).auctions.map(attachTimeUntilEnd);
 
       var saleLeast = saleAuctions.sort(function(a, b) {
         return a.current_price - b.current_price;
@@ -99,7 +88,7 @@ router.get('/sale', function(req, res, next) {
   request('https://api.cryptokitties.co/auctions?offset=&limit=1000&type=sale&status=open', function(err, respSale) {
       var bodySale = JSON.parse(respSale.body);
 
-      var sale = bodySale.auctions.sort(function(a, b) {
+      var sale = bodySale.auctions.map(attachTimeUntilEnd).sort(function(a, b) {
         return a.current_price - b.current_price;
       });
 
@@ -119,7 +108,7 @@ router.get('/sire', function(req, res, next) {
   request('https://api.cryptokitties.co/auctions?offset=&limit=1000&type=sire&status=open', function(err, respSire) {
       var bodySire = JSON.parse(respSire.body);
 
-      var sire = bodySire.auctions.sort(function(a, b) {
+      var sire = bodySire.auctions.map(attachTimeUntilEnd).sort(function(a, b) {
         return a.current_price - b.current_price;
       });
 
