23526982e8f3352b41f27e3743ecc8cf1a2fd982
Christopher Scott <cscott@iqt.org>
2017-11-30 23:16:49 -0800

    dont know what I am doing, but seems to work
    needs refactoringlol

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var moment = require('moment');
 var ethPrice = 520;
 
 function getEthPrice() {
-  request('https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=CAD', function(err, resp) {
+  request('https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=CAD', function (err, resp) {
     ethPrice = parseInt(JSON.parse(resp.body)[0].price_cad, 10);
   });
 }
@@ -15,120 +15,170 @@ getEthPrice();
 setInterval(getEthPrice, 1000 * 60 * 15); // Every 15 minutes
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
-  request('https://api.cryptokitties.co/auctions?offset=&limit=1000&type=sale&status=open', function(err, respSale) {
-    request('https://api.cryptokitties.co/auctions?offset=&limit=1000&type=sire&status=open', function(err, respSire) {
+  //https://api.cryptokitties.co/auctions?offset=0&limit=12&type=sale&status=open&sorting=cheap&orderBy=current_price&orderDirection=asc
 
-      var saleAuctions = JSON.parse(respSale.body).auctions.map(function(cat) {
-        var timeNow = moment();
-        var catEndTime = moment(new Date(parseInt(cat.end_time, 10)))
-        cat.time_until_end = catEndTime.fromNow();
+  request('https://api.cryptokitties.co/auctions?offset=&limit=10&type=sale&status=open&sorting=cheap&orderBy=current_price&orderDirection=asc', function (err, respSaleCheap) {
+    request('https://api.cryptokitties.co/auctions?offset=&limit=10&type=sale&status=open&sorting=cheap&orderBy=current_price&orderDirection=desc', function (err, respSaleExpensive) {
+      request('https://api.cryptokitties.co/auctions?offset=&limit=10&type=sire&status=open&sorting=cheap&orderBy=current_price&orderDirection=asc', function (err, respSireCheap) {
+        request('https://api.cryptokitties.co/auctions?offset=&limit=10&type=sire&status=open&sorting=cheap&orderBy=current_price&orderDirection=desc', function (err, respSireExpensive) {
 
-        var endingWithin5Minutes = catEndTime.diff(timeNow, 'minutes') <= 30;
+          var saleLeast = JSON.parse(respSaleCheap.body).auctions.map(function (cat) {
+            var timeNow = moment();
+            var catEndTime = moment(new Date(parseInt(cat.end_time, 10)))
+            cat.time_until_end = catEndTime.fromNow();
 
-        if (endingWithin5Minutes) {
-          cat.ending_soon = true;
-        }
+            var endingWithin5Minutes = catEndTime.diff(timeNow, 'minutes') <= 30;
 
-        return cat;
+            if (endingWithin5Minutes) {
+              cat.ending_soon = true;
+            }
+
+            return cat;
+          });
+
+          var saleMost = JSON.parse(respSaleExpensive.body).auctions.map(function (cat) {
+            var timeNow = moment();
+            var catEndTime = moment(new Date(parseInt(cat.end_time, 10)))
+            cat.time_until_end = catEndTime.fromNow();
+
+            var endingWithin5Minutes = catEndTime.diff(timeNow, 'minutes') <= 30;
+
+            if (endingWithin5Minutes) {
+              cat.ending_soon = true;
+            }
+
+            return cat;
+          });
+
+          var sireLeast = JSON.parse(respSireCheap.body).auctions.map(function (cat) {
+            var timeNow = moment();
+            var catEndTime = moment(new Date(parseInt(cat.end_time, 10)))
+            cat.time_until_end = catEndTime.fromNow();
+
+            var endingWithin5Minutes = catEndTime.diff(timeNow, 'minutes') <= 30;
+
+            if (endingWithin5Minutes) {
+              cat.ending_soon = true;
+            }
+
+            return cat;
+          });
+
+          var sireMost = JSON.parse(respSireExpensive.body).auctions.map(function (cat) {
+            var timeNow = moment();
+            var catEndTime = moment(new Date(parseInt(cat.end_time, 10)))
+            cat.time_until_end = catEndTime.fromNow();
+
+            var endingWithin5Minutes = catEndTime.diff(timeNow, 'minutes') <= 30;
+
+            if (endingWithin5Minutes) {
+              cat.ending_soon = true;
+            }
+
+            return cat;
+          });
+
+          // var saleLeast = saleAuctions.sort(function (a, b) {
+          //   return a.current_price - b.current_price;
+          // }).slice(0, 10);
+
+          // var saleMost = saleAuctions.sort(function (b, a) {
+          //   return a.current_price - b.current_price;
+          // }).slice(0, 10);
+
+          // var saleEndSoon = saleAuctions.sort(function (a, b) {
+          //   return a.end_time - b.end_time;
+          // }).slice(0, 10);
+
+          // var saleEndPrice = saleAuctions.sort(function (a, b) {
+          //   return a.end_price - b.end_price;
+          // }).slice(0, 10);
+
+          // var sireLeast = sireAuctions.sort(function (a, b) {
+          //   return a.current_price - b.current_price;
+          // }).slice(0, 10);
+
+          // var sireMost = sireAuctions.sort(function (b, a) {
+          //   return a.current_price - b.current_price;
+          // }).slice(0, 10);
+
+          // var sireEndSoon = sireAuctions.sort(function (a, b) {
+          //   return a.end_time - b.end_time;
+          // }).slice(0, 10);
+
+          res.render('index', {
+            ethPrice: ethPrice,
+            lastUpdate: new Date(),
+            //  saleEndSoon: saleEndSoon,
+            saleLeast: saleLeast,
+            saleMost: saleMost,
+            //saleEndPrice: saleEndPrice,
+            // sireEndSoon: sireEndSoon,
+            sireLeast: sireLeast,
+            sireMost: sireMost,
+            title: 'Top 10s'
+          });
+        });
       });
+    });
+  });
+});
 
-      var sireAuctions = JSON.parse(respSire.body).auctions.map(function(cat) {
-        var timeNow = moment();
-        var catEndTime = moment(new Date(parseInt(cat.end_time, 10)))
-        cat.time_until_end = catEndTime.fromNow();
+/* GET home page. */
+router.get('/sale', function (req, res, next) {
 
-        var endingWithin5Minutes = catEndTime.diff(timeNow, 'minutes') <= 30;
+  request('https://api.cryptokitties.co/auctions?offset=&limit=1000&type=sale&status=open&sorting=cheap&orderBy=current_price&orderDirection=asc', function (err, respSale) {
 
-        if (endingWithin5Minutes) {
-          cat.ending_soon = true;
-        }
+    var sale = JSON.parse(respSale.body).auctions.map(function (cat) {
+      var timeNow = moment();
+      var catEndTime = moment(new Date(parseInt(cat.end_time, 10)))
+      cat.time_until_end = catEndTime.fromNow();
 
-        return cat;
-      });
+      var endingWithin5Minutes = catEndTime.diff(timeNow, 'minutes') <= 30;
 
-      var saleLeast = saleAuctions.sort(function(a, b) {
-        return a.current_price - b.current_price;
-      }).slice(0, 10);
+      if (endingWithin5Minutes) {
+        cat.ending_soon = true;
+      }
 
-      var saleMost = saleAuctions.sort(function(b, a) {
-        return a.current_price - b.current_price;
-      }).slice(0, 10);
+      return cat;
+    });
 
-      var saleEndSoon = saleAuctions.sort(function(a, b) {
-        return a.end_time - b.end_time;
-      }).slice(0, 10);
-
-      var saleEndPrice = saleAuctions.sort(function(a, b) {
-        return a.end_price - b.end_price;
-      }).slice(0, 10);
-
-      var sireLeast = sireAuctions.sort(function(a, b) {
-        return a.current_price - b.current_price;
-      }).slice(0, 10);
-
-      var sireMost = sireAuctions.sort(function(b, a) {
-        return a.current_price - b.current_price;
-      }).slice(0, 10);
-
-      var sireEndSoon = sireAuctions.sort(function(a, b) {
-        return a.end_time - b.end_time;
-      }).slice(0, 10);
-
-      res.render('index', {
-        ethPrice: ethPrice,
-        lastUpdate: new Date(),
-        saleEndSoon: saleEndSoon,
-        saleLeast: saleLeast,
-        saleMost: saleMost,
-        saleEndPrice: saleEndPrice,
-        sireEndSoon: sireEndSoon,
-        sireLeast: sireLeast,
-        sireMost: sireMost,
-        title: 'Top 10s'
-      });
+    res.render('all', {
+      ethPrice: ethPrice,
+      lastUpdate: new Date(),
+      kitties: sale,
+      title: 'For Sale'
     });
   });
 
 });
 
 /* GET home page. */
-router.get('/sale', function(req, res, next) {
+router.get('/sire', function (req, res, next) {
 
-  request('https://api.cryptokitties.co/auctions?offset=&limit=1000&type=sale&status=open', function(err, respSale) {
-      var bodySale = JSON.parse(respSale.body);
+  request('https://api.cryptokitties.co/auctions?offset=&limit=1000&type=sire&status=open&sorting=cheap&orderBy=current_price&orderDirection=asc', function (err, respSire) {
+    var sire = JSON.parse(respSire.body).auctions.map(function (cat) {
+      var timeNow = moment();
+      var catEndTime = moment(new Date(parseInt(cat.end_time, 10)))
+      cat.time_until_end = catEndTime.fromNow();
 
-      var sale = bodySale.auctions.sort(function(a, b) {
-        return a.current_price - b.current_price;
-      });
+      var endingWithin5Minutes = catEndTime.diff(timeNow, 'minutes') <= 30;
 
-      res.render('all', {
-        ethPrice: ethPrice,
-        lastUpdate: new Date(),
-        kitties: sale,
-        title: 'For Sale'
-      });
-  });
+      if (endingWithin5Minutes) {
+        cat.ending_soon = true;
+      }
 
-});
+      return cat;
+    });
 
-/* GET home page. */
-router.get('/sire', function(req, res, next) {
-
-  request('https://api.cryptokitties.co/auctions?offset=&limit=1000&type=sire&status=open', function(err, respSire) {
-      var bodySire = JSON.parse(respSire.body);
-
-      var sire = bodySire.auctions.sort(function(a, b) {
-        return a.current_price - b.current_price;
-      });
-
-      res.render('all', {
-        ethPrice: ethPrice,
-        lastUpdate: new Date(),
-        kitties: sire,
-        title: 'For Sire'
-      });
+    res.render('all', {
+      ethPrice: ethPrice,
+      lastUpdate: new Date(),
+      kitties: sire,
+      title: 'For Sire'
+    });
   });
 
 });
