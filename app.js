6dbfb4abff764e37787821e626c266059961fc3a
Devin McInnis <devin.mcinnis@gmail.com>
2017-12-02 00:57:02 -0800

    add analytics; remove egregious saleMost

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.locals.isProduction = process.env.NODE_ENV === 'production';
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
