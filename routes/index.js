cc4b10103cca84b9f7704adc4caf766733e82425
Devin McInnis <devin.mcinnis@gmail.com>
2017-12-04 20:57:33 -0800

    add pagination and page nav

--- routes/index.js
+++ routes/index.js
@@ -14,13 +14,14 @@ getEthPrice();
 setInterval(getEthPrice, 1000 * 60 * 15); // Every 15 minutes
 
 const oneEth = 1000000000000000000;
+const API_LIMIT = 100;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
   //https://api.cryptokitties.co/auctions?offset=0&limit=12&type=sale&status=open&sorting=cheap&orderBy=current_price&orderDirection=asc
   request('https://api.cryptokitties.co/auctions?offset=&limit=10&type=sale&status=open&sorting=cheap&orderBy=current_price&orderDirection=asc', function (err, respSaleCheap) {
-    request('https://api.cryptokitties.co/auctions?offset=&limit=100&type=sale&status=open&sorting=cheap&orderBy=current_price&orderDirection=desc', function (err, respSaleExpensive) {
+    request(`https://api.cryptokitties.co/auctions?offset=&limit=${API_LIMIT}&type=sale&status=open&sorting=cheap&orderBy=current_price&orderDirection=desc`, function (err, respSaleExpensive) {
       request('https://api.cryptokitties.co/auctions?offset=&limit=10&type=sire&status=open&sorting=cheap&orderBy=current_price&orderDirection=asc', function (err, respSireCheap) {
         request('https://api.cryptokitties.co/auctions?offset=&limit=50&type=sire&status=open&sorting=cheap&orderBy=current_price&orderDirection=desc', function (err, respSireExpensive) {
 
@@ -39,11 +40,12 @@ router.get('/', function (req, res, next) {
 
           res.render('index', {
             ethPrice: ethPrice,
+            headerTitle: 'Top 10s',
             saleLeast: saleLeast,
             saleMost: saleMost,
             sireLeast: sireLeast,
             sireMost: sireMost,
-            title: 'Top 10s'
+            title: 'CryptoKitties Tools - Top 10s'
           });
         });
       });
@@ -52,36 +54,54 @@ router.get('/', function (req, res, next) {
 });
 
 
-router.get('/sale', function (req, res, next) {
-  var offset = req.query.offset || '';
+router.get('/sale/:page?', function (req, res, next) {
+  var paramPage = req.params.page;
+  var isGoodPageNum = paramPage > 0;
+  var page = isGoodPageNum ? parseInt(paramPage, 10) : 0;
+  var offset = page * API_LIMIT;
   var search = req.query.search || '';
 
-  request(`https://api.cryptokitties.co/auctions?offset=${offset}&search=${search}&limit=5000&type=sale&status=open&sorting=cheap&orderBy=current_price&orderDirection=asc`, function (err, respSale) {
+  request(`https://api.cryptokitties.co/auctions?offset=${offset}&search=${search}&limit=${API_LIMIT}&type=sale&status=open&sorting=cheap&orderBy=current_price&orderDirection=asc`, function (err, respSale) {
 
-    var sale = JSON.parse(respSale.body).auctions;
+    var parsedResp = JSON.parse(respSale.body);
+    var totalOverLimit = parsedResp.total > offset;
+    var sale = parsedResp.auctions;
 
     res.render('all', {
       ethPrice: ethPrice,
+      headerTitle: 'For Sale',
       kitties: sale,
-      title: 'For Sale'
+      nextPage: totalOverLimit ? page + 1 : null,
+      pageType: 'sale',
+      prevPage: isGoodPageNum ? page - 1 : null,
+      title: 'CryptoKitties Tools - For Sale'
     });
   });
 
 });
 
 
-router.get('/sire', function (req, res, next) {
-  var offset = req.query.offset || '';
+router.get('/sire/:page?', function (req, res, next) {
+  var paramPage = req.params.page;
+  var isGoodPageNum = paramPage > 0;
+  var page = isGoodPageNum ? parseInt(paramPage, 10) : 0;
+  var offset = page * API_LIMIT;
   var search = req.query.search || '';
 
-  request(`https://api.cryptokitties.co/auctions?offset=${offset}&search=${search}&limit=5000&type=sire&status=open&sorting=cheap&orderBy=current_price&orderDirection=asc`, function (err, respSire) {
+  request(`https://api.cryptokitties.co/auctions?offset=${offset}&search=${search}&limit=${API_LIMIT}&type=sire&status=open&sorting=cheap&orderBy=current_price&orderDirection=asc`, function (err, respSire) {
 
-    var sire = JSON.parse(respSire.body).auctions;
+    var parsedResp = JSON.parse(respSire.body);
+    var totalOverLimit = parsedResp.total > offset;
+    var sire = parsedResp.auctions;
 
     res.render('all', {
       ethPrice: ethPrice,
+      headerTitle: 'For Sire',
       kitties: sire,
-      title: 'For Sire'
+      nextPage: totalOverLimit ? page + 1 : null,
+      pageType: 'sire',
+      prevPage: isGoodPageNum ? page - 1 : null,
+      title: 'CryptoKitties Tools - For Sire'
     });
   });
 
@@ -94,23 +114,37 @@ function formatClockCat(kitty) {
     kitty: {
       generation: kitty.generation,
       id: kitty.id,
-      image_url: kitty.image_url
+      image_url: kitty.image_url,
+      status: {
+        cooldown_index: kitty.status.cooldown_index
+      }
     }
   };
 }
 
-router.get('/clock', function (req, res, next) {
-    request('https://api.cryptokitties.co/kitties?offset=0&limit=1000&owner_wallet_address=0x06012c8cf97bead5deae237070f9587f8e7a266d&sorting=cheap&orderBy=current_price&orderDirection=asc', function (err, respClock) {
+router.get('/clock/:pageId?', function (req, res, next) {
+  var paramPage = req.params.page;
+  var isGoodPageNum = paramPage > 0;
+  var page = isGoodPageNum ? parseInt(paramPage, 10) : 0;
+  var offset = page * API_LIMIT;
+  var search = req.query.search || '';
 
-      var clock = JSON.parse(respClock.body).kitties.map(formatClockCat);
+  request(`https://api.cryptokitties.co/kitties?offset=${offset}&limit=${API_LIMIT}&owner_wallet_address=0x06012c8cf97bead5deae237070f9587f8e7a266d&sorting=cheap&orderBy=current_price&orderDirection=asc`, function (err, respClock) {
 
-      res.render('all', {
-        ethPrice: ethPrice,
-        kitties: clock,
-        title: 'Clock Cats'
-      });
+    var parsedResp = JSON.parse(respClock.body);
+    var totalOverLimit = parsedResp.total > offset;
+    var clock = parsedResp.kitties.map(formatClockCat);
+
+    res.render('all', {
+      ethPrice: ethPrice,
+      headerTitle: 'Clock Cats',
+      kitties: clock,
+      nextPage: totalOverLimit ? page + 1 : null,
+      pageType: 'clock',
+      prevPage: isGoodPageNum ? page - 1 : null,
+      title: 'CryptoKitties Tools - Clock Cats'
     });
-
   });
+});
 
 module.exports = router;
